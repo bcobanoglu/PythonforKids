91798b75454d7b0c525e3c3381b51673de357638
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2020-07-01 12:30:27 +0300

    Add files via upload

---
+++ Bolum10/Proje10_2.py
@@ -0,0 +1,14 @@
+"""Bülent Çobanoğlu, Çocuklar için Python
+Proje 10.2. 
+Girilen Türkçe günün İngilizce karşılığını veren Türkçe – İngilizce bir sözlük programı kodlayalım.
+
+"""
+ara = input("Türkçe gün adı.:")
+trEn = {"pazartesi": "monday", "salı": "tuesday", 
+        "çarşamba": "wednesday", "perşembe": "thursday",
+        "cuma": "friday", "cumartesi": "saturday",
+        "pazar": "sunday"}
+
+
+print("İngilizcesi.:", end=" ")
+print(trEn.get(ara, "Bu kelime sözlükte yoktur!"))
