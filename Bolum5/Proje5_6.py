fd0e38f0014c339721c20075a15c3290899c0ab8
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2021-04-15 19:32:11 +0300

    Update Proje5_6.py

--- Bolum5/Proje5_6.py
+++ Bolum5/Proje5_6.py
@@ -1,21 +1,11 @@
 """ Çocuklar için Python, Bülent Çobanoğlu
-Proje 5.6. 
-100’lük sisteme göre girilen başarı notunu yanda verilen tabloya göre harfli sisteme çeviren programı kodlayınız. 
+Proje 5.6. Park Ücreti
 """
-Nt=int(input('Gir Notu.: '))
-print ("Ders Notu.:")
-if Nt >= 90 and Nt<=100:
-    print ("A")
-elif Nt >= 80 and Nt<=89:
-    print ("B")
-elif Nt >= 70 and Nt<=79:
-    print ("C")
-elif Nt >= 60 and Nt<=69:
-    print ("D")
-elif Nt >= 50 and Nt<=59:
-    print ("E")
-elif Nt >= 0 and Nt<=49:
-    print ("F")
+Saat = float(input('Kalma Süresi.: '))
+if Saat <= 2:
+	Ucret = 5
+elif Saat <= 5:
+	Ucret = 10
 else:
-    print ("Lütfen 0-100 arası not giriniz!")
-
+	Ucret = 15
+print ("Park Ücreti.:", Ucret, "TL")
