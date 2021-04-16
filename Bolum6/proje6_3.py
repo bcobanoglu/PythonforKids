b54b6a6de87cebde4f0083ba6ccf192200043016
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2021-04-16 11:18:36 +0300

    Update proje6_3.py

--- Bolum6/proje6_3.py
+++ Bolum6/proje6_3.py
@@ -1,3 +1,8 @@
+"""Bülent Çobanoğlu, Çocuklar için Python,
+Proje 6.3. Çift Sayıların Toplamı
+0’ dan 20’ e kadar ki (20 dahil) çift sayıların toplamını ekranda gösteren programı
+for döngüsü ile kodlayalım
+"""
 T = 0
 for a in range(0,22,2):
     T = T + a
