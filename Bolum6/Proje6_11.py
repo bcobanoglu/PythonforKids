b1404d217168caa36f7857da1b7f296a9c28ccc3
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2021-04-16 11:01:26 +0300

    Update and rename Proje6_9.py to Proje6_11.py

--- Bolum6/Proje6_11.py
+++ Bolum6/Proje6_11.py
@@ -1,6 +1,6 @@
-"""
-Proje 9. Çıktı deseni oluşturma: 
-Yandaki(Çıktıdaki) dik üçgen desenini ekrana basacak/yazacak programı iç içe döngü kullanarak  kodlayalım.
+"""Çocuklar için Python, Bülent Çobanoğlu
+Proje 11. Çıktı deseni oluşturma:
+Dik üçgen desenini ekrana basacak/yazacak programı iç içe döngü kullanarak  kodlayalım.
 """
 #dıştaki döngü: m
 for m in range (1,6): 
