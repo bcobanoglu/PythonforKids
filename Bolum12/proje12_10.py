bed39b1dfc08e9f410b5428297ab550998566449
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2021-04-17 21:45:50 +0300

    Add files via upload

---
+++ Bolum12/proje12_10.py
@@ -0,0 +1,72 @@
+# Hangman oyunu
+from random import choice
+liste = ["python", "swift", "java", "ruby", "php", "basic", "cobol", "delphi" ]
+#Listeden rasgele bir kelime seç
+hangman = ['''
+             +-+
+             O |
+            /|\|
+            / \|
+              === ''','''
+             +-+
+             O |
+            /|\|
+            /  |
+             === ''','''
+             +-+
+             O |
+            /|\|
+               |
+              === ''',''' 
+             +-+
+             O |
+            /| |
+               |
+              === ''','''
+             +-+
+             O |
+             | |
+               |
+              === ''','''
+             +-+
+             O |
+               |
+               |
+              === ''','''
+             +-+
+               |
+               |
+               |
+              === ''']
+kelime = choice(liste)
+dogruHarf = []
+yanlisHarf = []
+hak = len(hangman)
+while hak > 0:
+    out = ""
+    for h in kelime:
+        if h in dogruHarf:
+            out = out + h
+        else:
+            out = out + "_"
+    if out == kelime:
+        break
+
+    print("Kelime: ", out)  
+    #print(hak, "hak kaldı")
+    print (hangman[hak-1])
+    girHarf = input()
+    if girHarf in dogruHarf or girHarf in yanlisHarf:
+        print("Zaten ", girHarf, "girilmişti")
+    elif girHarf in kelime:
+        print("Doğru")
+        dogruHarf.append(girHarf)
+    else:
+        print("Yanlış")
+        hak = hak - 1
+        yanlisHarf.append(girHarf)
+
+if hak:
+    print("Tebrikler, Evet : ", kelime)
+else:
+    print("Maalesef kelimemiz : ", kelime)
