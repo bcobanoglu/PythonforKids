09dc05e55339471654933a3878603844bdef82b5
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2023-07-05 10:36:32 +0300

    Add files via upload

---
+++ turtle_kare.py
@@ -0,0 +1,12 @@
+from turtle import *
+shape("turtle") # kaplumbağa şekli seçildi
+pensize(6)      # kalem kalınlığı
+forward(100)    # İleri Git(100 adım)
+right(90)       # Sağa dön (90 derece)
+forward(100)    # İleri Git(100 adım)
+right(90)       # Sağa dön (90 derece)
+forward(100)    # İleri Git (100 adım)
+right(90)       # Sağa dön (90 derece)
+forward(100)    # İleri Git (100 adım)
+right(90)       # Sağa dön (90 derece)
+done()          # Dur
