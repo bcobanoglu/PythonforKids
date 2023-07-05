f72ad691b14e5aa1fb0e06737c1d52150538ea1f
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2023-07-05 11:12:06 +0300

    Add files via upload

---
+++ besgen_ciz.py
@@ -0,0 +1,14 @@
+from turtle import *
+shape("turtle") # kaplumbağa şekli seçildi
+pensize(6)      # kalem kalınlığı
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+forward(100)
+left(72)
+done()          # Dur
