00775a0fa4407899fc40642f00a15be445ae98eb
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2023-07-05 11:01:04 +0300

    Add files via upload

---
+++ ucgen_ciz.py
@@ -0,0 +1,10 @@
+from turtle import *
+shape("turtle") # kaplumbağa şekli seçildi
+pensize(6)      # kalem kalınlığı
+forward(100)    # İleri Git(100 adım)
+right(120)      # Sağa dön (120 derece)
+forward(100)    # İleri Git(100 adım)
+right(120)      # Sağa dön (120 derece)
+forward(100)    # İleri Git(100 adım)
+right(120)      # Sağa dön (120 derece)
+done()          # Dur
