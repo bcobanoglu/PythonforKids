f64895000029301bb81efaa1e2f7dab1da69ea9c
Bülent Çobanoğlu <37271016+bcobanoglu@users.noreply.github.com>
2023-07-05 10:48:48 +0300

    Add files via upload

---
+++ dikdortgen_ciz.py
@@ -0,0 +1,12 @@
+from turtle import *
+shape("turtle") # kaplumbağa şekli seçildi
+pensize(6)      # kalem kalınlığı
+forward(50)    # İleri Git(50 adım)
+right(90)       # Sağa dön (90 derece)
+forward(200)    # İleri Git(200 adım)
+right(90)       # Sağa dön (90 derece)
+forward(50)    # İleri Git (50 adım)
+right(90)       # Sağa dön (90 derece)
+forward(200)    # İleri Git (200 adım)
+right(90)       # Sağa dön (90 derece)
+done()          # Dur
